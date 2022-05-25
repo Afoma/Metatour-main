449e9e98faf60e7432528062579f917e81bde29e
Afoma Orji <antoinetteorji@gmail.com>
2022-05-25 03:27:46 -0700

    aligned the claim nft button to the right

--- src/components/Claim.js
+++ src/components/Claim.js
@@ -5,15 +5,21 @@ export const ClaimButton = () => {
   const nftDrop = useNFTDrop("0xA9F8815255E2fDC1CB2E164dE1bEedfD815D421B");
   const address = useAddress();
   return (
-    <div>
+    <div style=
+      {{
+        textAlign: "right",
+      }}>
       {address ? (
-        <button onClick={() => nftDrop?.claim(1)}>Claim 1</button>
+        <button style={{
+          backgroundColor: "#D81E5B",
+          width: "100px",
+          height: "50px",
+          textAlign: "center",
+          borderRadius: "10%",
+  }
+  }
+        onClick={() => nftDrop?.claim(1)}>Claim 1</button>
       ) : (
-        <div style={{
-            display: "flex",
-            justifyItem: "flex-end",
-        }
-        }>
             <button style={{
                 backgroundColor: "#D81E5B",
                 width: "100px",
@@ -24,8 +30,7 @@ export const ClaimButton = () => {
         }
          onClick={connectWithMetamask}>Claim NFT</button>
 
-        </div>
-)}
+ )}
     </div>
   );
 };
